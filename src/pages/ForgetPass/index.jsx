cfa78b4b8acc037e49849e149925d5187453b573
NguyenVoTien <tienvo28052000@gmail.com>
2024-06-25 14:57:52 +0700

    fix: responsive

--- src/pages/ForgetPass/index.jsx
+++ src/pages/ForgetPass/index.jsx
@@ -1,6 +1,7 @@
 import API from "common/api/api";
 import { Button, Text } from "components";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const ForgetPage = () => {
@@ -32,29 +33,40 @@ const ForgetPage = () => {
   };
 
   return (
-    <form onSubmit={handleResetPass}>
-      <div className="flex flex-col space-y-4">
-        <Text
-          htmlFor="email"
-          className="block text-sm font-semibold text-gray-900"
-        >
-          Email
-        </Text>
-        <input
-          id="email"
-          type="email"
-          className="min-w-0 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          placeholder="Username or Email..."
-          onChange={handleChangeEmail}
-        />
-        <Button
-          type="submit"
-          className="mt-2 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Reset
-        </Button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleResetPass}>
+        <div className="flex flex-col space-y-4">
+          <Text
+            htmlFor="email"
+            className="block text-sm font-semibold text-gray-900"
+          >
+            Email
+          </Text>
+          <input
+            id="email"
+            type="email"
+            className="min-w-0 block w-[50%] sm:w-3/4 md:w-1/2 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-sm"
+            placeholder="Username or Email..."
+            onChange={handleChangeEmail}
+          />
+          <Button
+            type="submit"
+            className="mt-2 w-[15%] sm:w-1/4 md:w-1/5 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Send Forget Password
+          </Button>
+        </div>
+      </form>
+      <Link
+        className="mt-2 w-[15%] sm:w-1/4 md:w-1/5 flex 
+           justify-center py-2 px-4 border border-transparent rounded-md 
+            shadow-sm text-sm font-medium text-gray-600 bg-gray-100
+          hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        to="/login"
+      >
+        Back
+      </Link>
+    </>
   );
 };
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -55,145 +55,144 @@ const Login = () => {
     <>
       <form
         onSubmit={handleSubmit}
-        className="bg-white-A700 h-[1117px] mx-auto p-[116px] md:px-10 sm:px-5 relative w-full"
+        className="bg-white h-[1117px] mx-auto p-[116px] md:p-10 sm:p-5 relative w-full"
       >
         <Img
-          className="absolute h-[519px] left-[13%] object-cover top-[14%] w-[28%]"
+          className="absolute h-[519px] left-[13%] object-cover top-[14%] w-[28%] md:w-[40%] sm:w-[60%]"
           src="images/img_31612531.png"
           alt="31612531"
         />
-        <div className="absolute bg-gray-100 flex flex-col h-max inset-[0] items-end justify-center m-auto outline outline-[1px] outline-black-900_42 p-3.5 md:px-5 rounded-[16px] w-2/5">
+        <div className="absolute bg-gray-100 flex flex-col h-max inset-[0] items-end justify-center m-auto outline outline-[1px] outline-black p-3.5 md:p-5 rounded-[16px] w-2/5 md:w-3/5 sm:w-4/5">
           <div className="flex flex-col justify-start mb-8 mr-4 w-[89%] md:w-full">
-            <div className="flex flex-col font-cocogoose gap-[18px] items-start justify-start md:ml-[0] ml-[199px] w-[67%] md:w-full">
+            <div className="flex flex-col font-cocogoose gap-[18px] items-start justify-start md:ml-0 ml-[199px] w-[67%] md:w-full">
               <div className="flex sm:flex-col flex-row sm:gap-5 items-start justify-start w-full">
                 <Img
                   className="sm:flex-1 h-[126px] md:h-auto sm:mt-0 mt-3 object-cover w-[39%] sm:w-full"
                   src="images/img_balksz11.png"
                   alt="balkszEleven"
                 />
-                <div className="h-[23px] sm:ml-[0] ml-[137px] relative w-1/5 sm:w-full">
+                <div className="h-[23px] sm:ml-0 ml-[137px] relative w-1/5 sm:w-full">
                   <a
                     href="javascript:"
-                    className="absolute h-full inset-[0] justify-center m-auto text-base text-black-900 w-max"
+                    className="absolute h-full inset-0 justify-center m-auto text-base text-black w-max"
                   >
-                    <Link to="/register" size="txtCOCOGOOSEDemiBold16">
+                    <Link to="/register" className="text-base font-semibold">
                       Register
                     </Link>
                   </a>
-                  <Line className="absolute bg-blue-800 bottom-[0] h-px left-[0] w-[69%]" />
+                  <div className="absolute bg-blue-800 bottom-0 h-px left-0 w-[69%]" />
                 </div>
                 <Img
-                  className="h-[18px] sm:ml-[0] ml-[7px] sm:mt-0 mt-0.5 w-[18px]"
+                  className="h-[18px] sm:ml-0 ml-2 sm:mt-0 mt-0.5 w-[18px]"
                   src="images/img_arrowleft.svg"
                   alt="arrowleft"
                 />
               </div>
               <Text
-                className="text-4xl sm:text-[32px] md:text-[34px] text-black-900"
+                className="text-4xl sm:text-2xl sm:text-center text-black"
                 size="txtCOCOGOOSEDemiBold36"
               >
                 bylogin
               </Text>
             </div>
             <Text
-              className="leading-[108.10%] mr-[38px] mt-[15px] text-center text-gray-600 text-sm w-[94%] sm:w-full"
+              className="leading-[108.10%] mt-4 text-center text-gray-600 text-sm w-full sm:w-full"
               size="txtSourceSansProSemiBold14"
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac
               tortor volutpat, vulputate massa non, feugiat tellus. Proin ac
               neque et felis bibendum varius.
             </Text>
-            <div className="flex flex-col font-sourcesanspro items-center justify-start md:ml-[0] ml-[47px] mt-[29px] w-[76%] md:w-full">
-              <div className="h-[73px] md:h-[89px] mt-[27px] relative w-full">
-                <div className="absolute bottom-[0] md:h-[62px] h-[66px] inset-x-[0] mx-auto w-full">
-                  <div className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-[0] items-start justify-center m-auto p-[17px] rounded w-full">
+            <div className="flex flex-col font-sourcesanspro items-center justify-start mt-7 w-full ">
+              <div className="h-[73px] md:h-[89px] mt-7 relative w-full">
+                <div className="absolute bottom-0 md:h-[62px] h-[66px] inset-x-0 mx-auto w-full">
+                  <div className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-0 items-start justify-center m-auto p-4 rounded w-full">
                     <input
-                      className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-[0] items-start justify-center m-auto p-[17px] rounded w-full"
+                      className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-0 items-start justify-center p-4 rounded w-full"
                       size="txtSourceSansProSemiBold14Gray500"
                       placeholder="Username or Email..."
                       onChange={handleChangeEmail}
                     />
                   </div>
-                  <div className="absolute bg-gray-100 h-[7px] left-[4%] top-[0] w-[28%]"></div>
+                  <div className="absolute bg-gray-100 h-[7px] left-[4%] top-0 w-[28%]" />
                 </div>
                 <Text
-                  className="absolute left-[12%] text-black-900 text-center text-sm top-[0]"
+                  className="absolute left-[12%] text-black text-center text-sm top-0"
                   size="txtSourceSansProSemiBold14Black900"
                 >
                   Email
                 </Text>
               </div>
-              <div className="h-[73px] md:h-[89px] mt-[27px] relative w-full">
-                <div className="absolute bottom-[0] md:h-[62px] h-[66px] inset-x-[0] mx-auto w-full">
-                  <div className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-[0] items-start justify-center m-auto p-[17px] rounded w-full">
+              <div className="h-[73px] md:h-[89px] mt-7 relative w-full">
+                <div className="absolute bottom-0 md:h-[62px] h-[66px] inset-x-0 mx-auto w-full">
+                  <div className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-0 items-start justify-center m-auto p-4 rounded w-full">
                     <input
-                      className="absolute bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-[0] items-start justify-center m-auto p-[17px] rounded w-full"
+                      className="absolute selection:bg-gray-100 border border-gray-400 border-solid flex flex-col h-max inset-0 items-start justify-center p-4 rounded w-full"
                       size="txtSourceSansProSemiBold14Gray500"
                       placeholder="********************"
                       onChange={handleChangePassword}
                     />
                   </div>
-                  <div className="absolute bg-gray-100 h-[7px] left-[4%] top-[0] w-[28%]"></div>
+                  <div className="absolute bg-gray-100 h-[7px] left-[4%] top-0 w-[28%]" />
                 </div>
                 <Text
-                  className="absolute left-[12%] text-black-900 text-center text-sm top-[0]"
+                  className="absolute left-[12%] text-black text-center text-sm top-0"
                   size="txtSourceSansProSemiBold14Black900"
                 >
                   Password
                 </Text>
               </div>
-              <div className="flex flex-row items-start justify-between mt-[17px] w-[96%] md:w-full">
+              <div className="flex flex-row items-start justify-between mt-4 w-full">
                 <CheckBox
                   className="font-semibold text-center text-sm"
-                  inputClassName="border border-gray-400 border-solid h-4 mr-[5px] w-4"
+                  inputClassName="border border-gray-400 h-4 mr-1 w-4"
                   name="rememberme"
                   id="rememberme"
                   label="Remember me"
-                ></CheckBox>
+                />
                 <button
-                  className="text-black-900 text-center text-sm"
-                  size="txtSourceSansProSemiBold14Black900"
+                  className="text-black text-center text-sm"
                   onClick={handleForgotPassword}
                 >
                   Forgot Password?
                 </button>
               </div>
               <Button
-                className="cursor-pointer font-semibold min-w-[435px] sm:min-w-full mt-3.5 text-base text-center"
+                className="cursor-pointer font-semibold mt-3 text-base text-center w-full"
                 onClick={handleSubmit}
               >
                 Login
               </Button>
             </div>
-            <div className="flex sm:flex-col flex-row font-sourcesanspro gap-[15px] items-center justify-start md:ml-[0] ml-[30px] mt-[25px] w-[81%] md:w-full">
+            <div className="flex sm:flex-col flex-row font-sourcesanspro gap-4 items-center justify-start mt-6 w-full">
               <div
-                className="common-pointer bg-blue-A200 flex flex-row gap-[25px] items-center justify-start p-0.5 w-[49%] sm:w-full"
+                className="common-pointer bg-blue-500 flex flex-row gap-4 items-center justify-start p-1 w-full sm:w-full"
                 onClick={() => googleSignIn()}
               >
-                <div className="bg-white-A700 flex flex-col items-start justify-end p-1.5 w-1/5">
+                <div className="bg-white flex flex-col items-start justify-end p-1.5 w-1/5">
                   <Img
-                    className="h-[26px] md:h-auto object-cover w-[26px]"
+                    className="h-[26px] object-cover w-[26px]"
                     src="images/img_googleglogo.png"
                     alt="googleglogo"
                   />
                 </div>
                 <Text
-                  className="text-center text-sm text-white-A700"
+                  className="text-center text-sm text-white w-full"
                   size="txtSourceSansProSemiBold14WhiteA700"
                 >
                   Sign in with Google
                 </Text>
               </div>
-              <div className="bg-indigo-600 flex flex-row gap-[25px] items-center justify-center p-0.5 w-[49%] sm:w-full">
-                <div className="bg-white-A700 flex flex-col items-center justify-end p-1 w-1/5">
+              <div className="bg-indigo-600 flex flex-row gap-4 items-center justify-center p-1 w-full sm:w-full">
+                <div className="bg-white flex flex-col items-center justify-end p-1 w-1/5">
                   <Img
-                    className="h-[30px] md:h-auto object-cover w-[43%]"
+                    className="h-[30px] object-cover w-[43%]"
                     src="images/img_facebook2logo.png"
                     alt="facebook2logo"
                   />
                 </div>
                 <Text
-                  className="mr-[26px] text-center text-sm text-white-A700"
+                  className="text-center text-sm text-white w-full"
                   size="txtSourceSansProSemiBold14WhiteA700"
                 >
                   Sign in with Facebook
